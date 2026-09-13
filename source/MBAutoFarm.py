671ce71c17693b22b02d3adce730e8dc786ca595
koudai <786410175@qq.com>
2026-09-13 19:33:12 +0800

    添加钓鱼功能

--- source/MBAutoFarm.py
+++ source/MBAutoFarm.py
@@ -8,7 +8,7 @@ from Theme import QtTheme_rc  # noqa: F401  注册 qrc 资源（图标、qss 等
 # import taskControl
 from taskControl import taskControl
 
-APP_VERSION = "1.0.3"
+APP_VERSION = "1.0.4"
 APP_CONFIG_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "config.json")
 
 if __name__ == "__main__":
